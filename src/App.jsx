fabd14797f1c1e9b2bc62f81fa1fe111f652ed29
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-14 15:10:01 -0400

    posts now show up on the browser

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Content } from "./Content";
 import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 
 function App() {
   return (
     <div>
       <Header />
       <Signup />
+      <Login />
+      <LogoutLink />
       <Content />
       <Footer />
     </div>
@@ -17,24 +21,3 @@ function App() {
 }
 
 export default App;
-
-//     {
-//   id: 1,
-//   title: "My Awesome Vacation",
-//   body: "Just got home from the best vacation ever",
-//   image_url:
-//     "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/05/10054553/12-Flexible-Companies-That-Help-Pay-for-Your-Vacation.jpg",
-// },
-// {
-//   id: 2,
-//   title: "I just got home from the beach",
-//   body: "Such great weather today",
-//   image_url: "https://images.nationalgeographic.org/image/upload/v1638889927/EducationHub/photos/pebble-beach.jpg",
-// },
-// {
-//   id: 3,
-//   title: "New job!!",
-//   body: "I just got a new job!!!!",
-//   image_url:
-//     "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2022/06/software_engineer.jpeg.jpg",
-// },
--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,16 +1,28 @@
+import axios from "axios";
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/posts.json", params).then((response) => {
+      console.log(response.data);
+      event.target.reset();
+      window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
+    });
+  };
+
   return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          Title: <input type="text" />
+          Title: <input name="title" type="text" />
         </div>
         <div>
-          Body: <input type="text" />
+          Body: <input name="body" type="text" />
         </div>
         <div>
-          Image: <input type="text" />
+          Image: <input name="image" type="text" />
         </div>
         <button type="submit">Create Post</button>
       </form>
