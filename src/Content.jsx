374ee049847f255d72f360a64676b5e8d67e8ad7
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-15 14:13:30 -0400

    update post works

--- src/Content.jsx
+++ src/Content.jsx
@@ -33,6 +33,23 @@ export function Content() {
     setCurrentPost(post);
   };
 
+  const handleUpdatePost = (id, params, successCallback) => {
+    console.log("handleUpdatePost", params);
+    axios.patch(`http://localhost:3000/posts/${id}.json`, params).then((response) => {
+      setPosts(
+        posts.map((post) => {
+          if (post.id === response.data.id) {
+            return response.data;
+          } else {
+            return post;
+          }
+        })
+      );
+      successCallback();
+      handleClose();
+    });
+  };
+
   const handleClose = () => {
     setIsPostsShowVisible(false);
   };
@@ -47,7 +64,7 @@ export function Content() {
       <PostsNew onCreatePost={handleCreatePost} />
       <PostsIndex posts={posts} onShowPost={handleShowPost} />
       <Modal show={isPostsShowVisible} onClose={handleClose}>
-        <PostsShow post={currentPost} />
+        <PostsShow post={currentPost} onUpdatePost={handleUpdatePost} />
       </Modal>
     </div>
   );
--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,9 +1,28 @@
 export function PostsShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onUpdatePost(props.post.id, params, () => event.target.reset());
+  };
+
   return (
     <div id="posts-show">
       <h1>More Info</h1>
       <h3>Title: {props.post.title}</h3>
       <p>Body: {props.post.body}</p>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          Title: <input defaultValue={props.post.title} name="title" type="text" />
+        </div>
+        <div>
+          Body: <input defaultValue={props.post.body} name="body" type="text" />
+        </div>
+        <div>
+          Image: <input defaultValue={props.post.image} name="image" type="text" />
+        </div>
+        <button type="submit">Update Post</button>
+      </form>
     </div>
   );
 }
